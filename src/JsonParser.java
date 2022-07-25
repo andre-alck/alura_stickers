798d650a919bf4fd19e9727fd1840f51e25f8cc6
andre-alck <andrealck1@gmail.com>
2022-07-25 19:24:25 -0300

    fixing commits & branches - master

--- src/JsonParser.java
+++ src/JsonParser.java
@@ -14,7 +14,7 @@ public class JsonParser {
         Matcher matcher = REGEX_ITEMS.matcher(json);
         if (!matcher.find()) {
 
-            throw new IllegalArgumentException("Não encontrou items.");
+            throw new IllegalArgumentException("Não encontrou itens.");
         }
 
         String[] items = matcher.group(1).split("\\},\\{");
